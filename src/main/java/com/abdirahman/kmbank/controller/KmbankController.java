4933634867937f4ea86cd4cbf84f6c8378215d18
abdirahman <abdixcom@gmail.com>
2023-12-10 13:48:33 +0300

    Added hello method to KmbankController

--- src/main/java/com/abdirahman/kmbank/controller/KmbankController.java
+++ src/main/java/com/abdirahman/kmbank/controller/KmbankController.java
@@ -1,7 +1,19 @@
 package com.abdirahman.kmbank.controller;
 
+import com.abdirahman.kmbank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class KmbankController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello";
+    }
 }
