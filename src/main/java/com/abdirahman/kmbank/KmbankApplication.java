610dbc4c1f777c9d2ccf94dc3a15cff73cb9b9c8
abdirahman <abdixcom@gmail.com>
2023-12-10 06:27:39 +0300

    Initial commit

---
+++ src/main/java/com/abdirahman/kmbank/KmbankApplication.java
@@ -0,0 +1,13 @@
+package com.abdirahman.kmbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdirahman/kmbank/KmbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdirahman.kmbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
